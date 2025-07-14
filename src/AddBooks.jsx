d30ac21e3cfebf85fc09fc50820398cb28cfd9d2
Punit sinha <punitsinha495@gmail.com>
2025-07-14 14:23:50 +0530

    added slash

--- src/AddBooks.jsx
+++ src/AddBooks.jsx
@@ -39,7 +39,7 @@ export default function AddBooks({ onCreated }) {
     setError(null);
 
     try {
-      const res = await fetch("https://bookappapi-5smm.onrender.com/api/get/post", {
+      const res = await fetch("https://bookappapi-5smm.onrender.com/api/get/post/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
